89c139afbd929ddeb80001def5f0b9d336a17efa
Kevin Nathaniel <dchristov2005206@gmail.com>
2026-03-23 20:02:25 +0700

    updated timezone conversion to replace the col instead of creating a new one

--- notebooks/02_data_analysis.py
+++ notebooks/02_data_analysis.py
@@ -1,12 +1,12 @@
 # %%
 import polars as pl
 
-from src.utils.dt_utils import convert_to_timezone
+from ohlc_dss_model.utils.dt_utils import convert_to_timezone
 
 # %%
 data_path = "../data/raw/NAS100_30m.parquet"
 
 # %%
 df = pl.read_parquet(data_path)
-df = convert_to_timezone(df, col_suffix="NY")
+df = convert_to_timezone(df)
 print(df.head(5))
--- src/ohlc_dss_model/utils/dt_utils.py
+++ src/ohlc_dss_model/utils/dt_utils.py
@@ -7,14 +7,7 @@ def convert_to_timezone(
     dt_col: str = "DateTime",
     current_tz: str = "EET",
     target_tz: str = "America/New_York",
-    col_suffix: str | None = None,
 ) -> pl.DataFrame:
-    if col_suffix is None:
-        col_suffix = target_tz
-
     return df.with_columns(
-        pl.col(dt_col)
-        .dt.replace_time_zone(current_tz)
-        .dt.convert_time_zone(target_tz)
-        .alias(f"{dt_col}_{col_suffix}")
+        pl.col(dt_col).dt.replace_time_zone(current_tz).dt.convert_time_zone(target_tz)
     )
